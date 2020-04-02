b66b2567407c5868969dcd4439213875df56dec8
Semyon Komyagin <semyon_komyagin@epam.com>
2020-04-02 18:38:26 +0300

    Completed Homework #2. Refactored hard assertion tests

--- src/test/java/hw2/exercise2/AssertWebElementsOnPageTest.java
+++ src/test/java/hw2/exercise2/AssertWebElementsOnPageTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
+import org.testng.asserts.Assertion;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
@@ -12,89 +13,52 @@ import static org.testng.Assert.assertTrue;
 
 public class AssertWebElementsOnPageTest extends BaseTest {
 
-    // 2. Assert Browser title
-    @Test(priority = 1)
+    Assertion assertion = new Assertion();
+
+    @Test
     public void isIndexPageTitleCorrect() {
+        // 2. Assert Browser title
         assertEquals(driver.getTitle(), "Home Page", "Not expected Index page title");
-    }
-
-    // 3. Perform login
-    @Test(priority = 2)
-    public void isUserLoggedIn() {
+        // 3. Perform login
         assertTrue(isWebElementDisplayed(By.id("user-name")));
-    }
-
-    // 4. Assert User name in the left-top side of screen that user is loggedIn
-    @Test(priority = 3)
-    public void isCorrectUserNameDisplayed() {
+        // 4. Assert User name in the left-top side of screen that user is loggedIn
         assertEquals(getUserName(), "ROMAN IOVLEV", "Not expected logged user");
-    }
-
-    // 5. Open through the header menu Service -> Different Elements Page
-    @Test(priority = 4)
-    public void isDifferentElementsPageOpened() {
+        // 5. Open through the header menu Service -> Different Elements Page
         getWebElement(By.linkText("Service")).click();
         getWebElement(By.linkText("Different elements")).click();
         assertEquals(driver.getTitle(), "Different Elements", "Not expected Different elements" +
                 "page title");
-    }
-
-    // 6. Select checkboxes
-    @Test(priority = 5)
-    public void areCheckboxesSelected() {
+        // 6. Select checkboxes
         WebElement waterCheckbox = getWebElement(By.xpath("//label[normalize-space() = 'Water']"));
         WebElement windCheckbox = getWebElement(By.xpath("//label[normalize-space() = 'Wind']"));
         waterCheckbox.click();
         windCheckbox.click();
         assertTrue(waterCheckbox.isEnabled() && windCheckbox.isEnabled(), "Water and wind " +
                 "checkboxes are not selected");
-    }
-
-    // 7. Select radio
-    @Test(priority = 6)
-    public void isRadioButtonChecked() {
+        // 7. Select radio
         WebElement selenRadioButton = getWebElement(By.xpath("//label[normalize-space() = 'Selen']"));
         selenRadioButton.click();
         assertTrue(selenRadioButton.isEnabled());
-    }
-
-    // 8. Select in dropdown
-    @Test(priority = 7)
-    public void isCorrectElementInDropDownSelected() {
+        // 8. Select in dropdown
         WebElement dropDownMenu = getWebElement(By.cssSelector(".colors select"));
-        Select selectedElement =new Select(dropDownMenu);
+        Select selectedElement = new Select(dropDownMenu);
         selectedElement.selectByVisibleText("Yellow");
         assertEquals(selectedElement.getFirstSelectedOption().getText(), "Yellow",
                 "Not right element is selected");
-    }
-
-    /* 9. Assert that
+        /* 9. Assert that
         •	for each checkbox there is an individual log row and value is corresponded to the status of checkbox
         •	for radio button there is a log row and value is corresponded to the status of radio button
         •	for dropdown there is a log row and value is corresponded to the selected value.
 
-    */
-    @Test(priority = 8)
-    public void isWaterCheckboxIsDisplayedInLogRow() {
-        WebElement webElement = getWebElement(By.xpath("//li[contains(text(),'Water: condition changed to true')]"));
-        assertTrue(webElement.isDisplayed(), "Water checkbox is not displayed in log row");
+        */
+        WebElement webElementWater = getWebElement(By.xpath("//li[contains(text(),'Water: condition changed to true')]"));
+        assertTrue(webElementWater.isDisplayed(), "Water checkbox is not displayed in log row");
+        WebElement webElementWind = getWebElement(By.xpath("//li[contains(text(),'Wind: condition changed to true')]"));
+        assertTrue(webElementWind.isDisplayed(), "Wind checkbox is not displayed in log row");
+        WebElement webElementSelen = getWebElement(By.xpath("//li[contains(text(),'metal: value changed to  Selen')]"));
+        assertTrue(webElementSelen.isDisplayed(), "Selen radio button is not displayed in log row");
+        WebElement webElementYellow = getWebElement(By.xpath("//li[contains(text(),'Colors: value changed to Yellow')]"));
+        assertTrue(webElementYellow.isDisplayed(), "Dropdown selected color is not displayed in log row");
     }
 
-    @Test(priority = 9)
-    public void isWindCheckboxIsDisplayedInLogRow() {
-        WebElement webElement = getWebElement(By.xpath("//li[contains(text(),'Wind: condition changed to true')]"));
-        assertTrue(webElement.isDisplayed(), "Wind checkbox is not displayed in log row");
-    }
-
-    @Test(priority = 10)
-    public void isSelenRadioButtonIsDisplayedInLogRow() {
-        WebElement webElement = getWebElement(By.xpath("//li[contains(text(),'metal: value changed to  Selen')]"));
-        assertTrue(webElement.isDisplayed(), "Selen radio button is not displayed in log row");
-    }
-
-    @Test(priority = 11)
-    public void isDropdownSelectedColorIsDisplayedInLogRow() {
-        WebElement webElement = getWebElement(By.xpath("//li[contains(text(),'Colors: value changed to Yellow')]"));
-        assertTrue(webElement.isDisplayed(), "Dropdown selected color is not displayed in log row");
-    }
 }
